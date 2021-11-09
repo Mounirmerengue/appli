5cc3a258cfc4f5b3c401cdb8195f532f9700827b
Mounirmerengue <m.aouhar@gmail.com>
2021-11-09 10:57:52 +0100

    retour version initial

--- recap.php
+++ recap.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Récapitulatif produits</title>
+    <title>Récapitulatif des produits</title>
 </head>
 <body>
     <?php
