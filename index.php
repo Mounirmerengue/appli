f5c0c83b76ba3ca06f8fe997204bba8e118a830b
Mounirmerengue <m.aouhar@gmail.com>
2021-11-09 14:16:41 +0100

    ajout d'une feuille de style

--- index.php
+++ index.php
@@ -4,31 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Ajout produits</title>
 </head>
+<header>
+    <nav id="nav">
+        <a href="recap.php"><i class="fas fa-cart-arrow-down"></i>  PANIER</a>
+    </nav>
+</header>
 <body>
     <h1>Ajouter un produit</h1>
     <form action="traitement.php" method="post">
         <p>
             Nom du produit :
-            <label>
+            <label class="nomP">
                 <input type="text" name="name">
             </label>
         </p>
         <p>
-            Prix du produit :
-            <label>
+             Prix du produit  : 
+            <label class="prix">
                 <input type="number" step="any" name="price">
             </label>
         </p>
         <p>
             Quantité désirée :
-            <label>
+            <label class="quantite">
                 <input type="number" name="qtt" value="1">
             </label>
         </p>
         <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
+            <input class="submitbut" type="submit" name="submit" value="Ajouter le produit">
         </p>
     </form>
 </body>
--- traitement.php
+++ traitement.php
@@ -4,7 +4,12 @@
     if(isset($_POST['submit'])){
 
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
-        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, [
+            "options" => [
+                "min_range" => 0
+            ],
+            "flags" => FILTER_FLAG_ALLOW_FRACTION
+        ]);
         $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
         if($name && $price && $qtt){
